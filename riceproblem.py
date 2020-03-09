dc98cc1cbc3fd9d76950f0f7a3be2b583e2e0465
SkrrtSkrrt652 <34437907+SkrrtSkrrt652@users.noreply.github.com>
2020-03-09 14:30:19 +0530

    Add files via upload

--- riceproblem.py
+++ riceproblem.py
@@ -4,6 +4,7 @@ rememberedStuff = {0: 1}
 
 def bigMod(base,exp,m):
     rememberedStuff[exp] = (base%m*rememberedStuff[exp-1])%m
+    del rememberedStuff[exp-1]
     return rememberedStuff[exp]
 
 # special modulo function that returns (1+r+r^2+r^3....+r^(n-1))%m
@@ -13,7 +14,7 @@ def sMod(R, N, M):
         modsum += bigMod(R,i,M)
     return modsum % M
 
-f = open("input-riceboard-8544.txt")
+f = open("input-riceboard-2b51.txt")
 g = open("output.txt", "w")
 T = int(f.readline())
 for t in range(T):
